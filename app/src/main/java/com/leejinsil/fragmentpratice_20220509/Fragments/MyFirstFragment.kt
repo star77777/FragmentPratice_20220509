c83a008ccd4a2cea60cba836bb56054a5b235cb0
star <quf2525@naver.com>
2022-05-09 10:09:45 +0900

    프래그먼트 클래스에 onactivityCreated함 수 오버라이딩 => 프래그먼트 이벤트 처리

--- app/src/main/java/com/leejinsil/fragmentpratice_20220509/Fragments/MyFirstFragment.kt
+++ app/src/main/java/com/leejinsil/fragmentpratice_20220509/Fragments/MyFirstFragment.kt
@@ -17,4 +17,13 @@ class MyFirstFragment :Fragment(){
         //루트에까지 파일을 붙일꺼니?
         return inflater.inflate(R.layout.fragment_myfirst,container, false)
     }
+
+    
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+//        이 프래그먼트를 들고있는 액티비티가 완전히 만들어진 시점
+//        (activity 생명주기 활용)> 다른 함수로 바꿔서 설명 예정
+        // 이벤트 처리,데이터 보여주기 등등 "동작"
+        //프래그먼트를 액티비티와 사용법이 비슷하다
+    }
 }

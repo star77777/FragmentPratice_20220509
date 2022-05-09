65b5fa41a34b18fb400e8ec017e24ae440031cb3
star <quf2525@naver.com>
2022-05-09 09:56:45 +0900

    프래그먼트 클래스에 onCreateView함 수 오버라이딩 => view를 프래그먼트에 붙여줬다.

--- app/src/main/java/com/leejinsil/fragmentpratice_20220509/Fragments/MyFirstFragment.kt
+++ app/src/main/java/com/leejinsil/fragmentpratice_20220509/Fragments/MyFirstFragment.kt
@@ -1,4 +1,20 @@
 package com.leejinsil.fragmentpratice_20220509.Fragments
 
-class MyFirstFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.leejinsil.fragmentpratice_20220509.R
+
+//()하니까 오류가 없어지는 걸 보니 안에 안적어줘도 된다
+class MyFirstFragment :Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //루트에까지 파일을 붙일꺼니?
+        return inflater.inflate(R.layout.fragment_myfirst,container, false)
+    }
 }
